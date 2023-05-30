e9959fee89142379b657b6165911aa65341078c8
Lucas Sas Brunschier <lucassas@live.de>
2023-05-30 07:47:12 +0000

    feat: null ls

--- configurations/nvim/init.lua
+++ configurations/nvim/init.lua
@@ -424,6 +424,16 @@ require("lazy").setup({
         'preservim/nerdcommenter',
         'ryanoasis/vim-devicons',
         'easymotion/vim-easymotion',
+        { 'jose-elias-alvarez/null-ls.nvim', config = function ()
+                local null_ls = require("null-ls")
+                null_ls.setup {
+                        sources = {
+                                null_ls.builtins.code_actions.gitsigns,
+                                null_ls.builtins.diagnostics.eslint,
+                        },
+                }
+                end, 
+        },
         { 'Darazaki/indent-o-matic',  config = true },
         { 'gbprod/yanky.nvim',        config = true, opts = {} },
         { 'simrat39/rust-tools.nvim', config = true },
